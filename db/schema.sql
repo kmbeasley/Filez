940c02be9d62bd3d44265605f9c142079ecbc6c7
Kyler Beasley <kylerbeasley7@gmail.com>
2026-06-05 10:48:14 -0500

    Creating table in Schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,19 @@
 -- TODO
+
+DROP TABLE IF EXISTS folders CASCADE;
+DROP TABLE IF EXISTS files;
+-- CREATE TABLE of folders.
+CREATE TABLE folders (
+  id serial PRIMARY KEY,
+  -- Vaules Cannot be Null. 
+  name text UNIQUE NOT NULL
+);
+create table files (
+  id serial PRIMARY KEY,
+  name text NOT NULL,
+  size integer NOT NULL,
+  -- I want to all folders with id when Deleted. 
+  folder_id integer NOT NULL REFERENCES folders(id) ON DELETE CASCADE,
+  -- Combo of them together is what makes it Unique. Different names 
+  UNIQUE (name, folder_id)
+);
